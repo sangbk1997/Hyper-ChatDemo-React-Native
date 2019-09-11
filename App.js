af26e17d99842cccf017861b499d917a167ae7d3
Nguyễn Bình Sang <sanghust97@gmail.com>
2019-09-11 13:58:59 +0700

    10/09/2019

--- App.js
+++ App.js
@@ -5,13 +5,14 @@ import {Provider} from 'react-redux';
 import {store} from './src/hyper-chat/_helpers';
 import AppContainer from "./src/hyper-chat/screens/land-page.screen";
 import FlashMessage from "react-native-flash-message";
+
 const styles = require('./src/static/css-app')
 
 export default function App() {
     return (
         <Provider store={store}>
             <AppContainer/>
-            <FlashMessage position="top" />
+            <FlashMessage position="top"/>
         </Provider>
     )
 }
--- src/hyper-chat/_constants/config.constants.js
+++ src/hyper-chat/_constants/config.constants.js
@@ -1,5 +1,5 @@
 export const configConstants = {
     HOST_PUSH_STREAM: '172.20.30.107',
-    PREFIX_APP_SERVER: "http://172.20.20.12:9000/",
+    PREFIX_APP_SERVER: "http://172.20.50.138:9000/",
     PREFIX_PUSH_STREAM: "ws://172.20.30.107/"
 }
